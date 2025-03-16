299dcdb6a2760919d9656d351bd7f3cedfac446c
O'Brien Otieno <obrienotieno@Brians-MacBook-Pro.local>
2025-03-16 11:00:39 +0300

    add password recovery

---
+++ models/password.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"time"
+
+	"digitaltrader/db"
+)
+
+type PasswordResetToken struct {
+	Email     string    `json:"email" gorm:"primaryKey"`
+	Token     string    `json:"token"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// CreatePasswordResetToken creates a new password reset token
+func CreatePasswordResetToken(token *PasswordResetToken) error {
+	db := db.InitDB()
+	if err := db.Create(&token).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetPasswordResetToken retrieves a password reset token by email
+func GetPasswordResetToken(token *PasswordResetToken, email string) error {
+	db := db.InitDB()
+	if err := db.Where("email = ?", email).First(&token).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdatePasswordResetToken updates an existing password reset token
+func UpdatePasswordResetToken(token *PasswordResetToken, email string) error {
+	db := db.InitDB()
+	if err := db.Where("email = ?", email).Updates(&token).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeletePasswordResetToken deletes a password reset token by email
+func DeletePasswordResetToken(email string) error {
+	db := db.InitDB()
+	if err := db.Where("email = ?", email).Delete(&PasswordResetToken{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetAllPasswordResetTokens retrieves all password reset tokens
+func GetAllPasswordResetTokens(tokens *[]PasswordResetToken) error {
+	db := db.InitDB()
+	if err := db.Find(&tokens).Error; err != nil {
+		return err
+	}
+	return nil
+}
