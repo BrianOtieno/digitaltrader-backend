957bf2045a08fd7477c65225c4dacfb5581e1e98
O'Brien Otieno <obrienotieno@Brians-MacBook-Pro.local>
2025-03-20 13:45:38 +0300

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -14,7 +14,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(cors.Middleware(cors.Config{
 		// Origins: "http://localhost",
-		Origins:         "https://quantum-cartel.com",
+		Origins:         "https://xxx.com",
 		Methods:         "GET, PUT, POST, DELETE, OPTIONS",
 		RequestHeaders:  "Origin, Authorization, Content-Type",
 		ExposedHeaders:  "",
@@ -24,7 +24,7 @@ func SetupRouter() *gin.Engine {
 	}))
 
 	// r.Use(cors.New(cors.Config{
-	// 	AllowOrigins: []string{"https://quantum-cartel.com"},
+	// 	AllowOrigins: []string{"https://xxx.com"},
 	// 	// AllowOrigins:     []string{"*"},
 	// 	AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
 	// 	AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
@@ -112,6 +112,69 @@ func SetupRouter() *gin.Engine {
 			protected.PUT("/:id", controllers.UpdateAddressController)
 			protected.DELETE("/:id", controllers.DeleteAddressController)
 			protected.GET("", controllers.GetAllAddressesController)
+
+			// chat room
+			protected.POST("", controllers.CreateChatRoomController)
+			protected.GET("/:id", controllers.GetChatRoomController)
+			protected.PUT("/:id", controllers.UpdateChatRoomController)
+			protected.DELETE("/:id", controllers.DeleteChatRoomController)
+			protected.GET("", controllers.GetAllChatRoomsController)
+
+			// cities
+			protected.POST("", controllers.CreateCityController)
+			protected.GET("/:id", controllers.GetCityController)
+			protected.PUT("/:id", controllers.UpdateCityController)
+			protected.DELETE("/:id", controllers.DeleteCityController)
+			protected.GET("", controllers.GetAllCitiesController)
+
+			// favourites
+			protected.POST("", controllers.CreateFavouriteController)
+			protected.GET("/:id", controllers.GetFavouriteController)
+			protected.PUT("/:id", controllers.UpdateFavouriteController)
+			protected.DELETE("/:id", controllers.DeleteFavouriteController)
+			protected.GET("", controllers.GetAllFavouritesController)
+
+			// flush
+			protected.POST("", controllers.CreateFlushController)
+			protected.GET("/:id", controllers.GetFlushController)
+			protected.PUT("/:id", controllers.UpdateFlushController)
+			protected.DELETE("/:id", controllers.DeleteFlushController)
+			protected.GET("", controllers.GetAllFlushesController)
+
+			// chat message
+			protected.POST("", controllers.CreateChatMessageController)
+			protected.GET("/:id", controllers.GetChatMessageController)
+			protected.PUT("/:id", controllers.UpdateChatMessageController)
+			protected.DELETE("/:id", controllers.DeleteChatMessageController)
+			protected.GET("", controllers.GetAllChatMessagesController)
+
+			// general
+			protected.POST("", controllers.CreateGeneralController)
+			protected.GET("/:id", controllers.GetGeneralController)
+			protected.PUT("/:id", controllers.UpdateGeneralController)
+			protected.DELETE("/:id", controllers.DeleteGeneralController)
+			protected.GET("", controllers.GetAllGeneralsController)
+
+			// language
+			protected.POST("", controllers.CreateLanguageController)
+			protected.GET("/:id", controllers.GetLanguageController)
+			protected.PUT("/:id", controllers.UpdateLanguageController)
+			protected.DELETE("/:id", controllers.DeleteLanguageController)
+			protected.GET("", controllers.GetAllLanguagesController)
+
+			// otp
+			protected.POST("", controllers.CreateOTPController)
+			protected.GET("/:id", controllers.GetOTPController)
+			protected.PUT("/:id", controllers.UpdateOTPController)
+			protected.DELETE("/:id", controllers.DeleteOTPController)
+			protected.GET("", controllers.GetAllOTPsController)
+
+			// payment
+			protected.POST("", controllers.CreatePaymentController)
+			protected.GET("/:id", controllers.GetPaymentController)
+			protected.PUT("/:id", controllers.UpdatePaymentController)
+			protected.DELETE("/:id", controllers.DeletePaymentController)
+			protected.GET("", controllers.GetAllPaymentsController)
 
 		}
 	}
